0841b2121cd9a9346c24aaf596a338ed965b85bf
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-06-28 13:10:55 +0000

    automatically backed up by learn

--- lib/user.rb
+++ lib/user.rb
@@ -2,9 +2,13 @@ class User
 
   attr_accessor :first_name, :last_name
 
-  def initialize(first_name, last_name)
-    @first_name = first_name
-    @last_name = last_name
+  def first_name(name)
+    @first_name = name
+    @first_name
+  end
+
+  def first_name=(name)
+    @first_name = name
   end
 
 end
